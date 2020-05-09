8dd5bb7c67f818b64126de4dff9ed75a191de122
Huyen <lvhuyen@yahoo.com>
2020-05-09 21:58:41 +1000

    code formatting

--- flink-metrics/flink-metrics-newrelic/src/main/java/org/apache/flink/metrics/newrelic/FlinkNewRelicReporter.java
+++ flink-metrics/flink-metrics-newrelic/src/main/java/org/apache/flink/metrics/newrelic/FlinkNewRelicReporter.java
@@ -18,50 +18,11 @@
 
 package org.apache.flink.metrics.newrelic;
 
-import org.apache.flink.metrics.CharacterFilter;
-import org.apache.flink.metrics.Counter;
-import org.apache.flink.metrics.Gauge;
-import org.apache.flink.metrics.Histogram;
-import org.apache.flink.metrics.Meter;
 import org.apache.flink.metrics.Metric;
 import org.apache.flink.metrics.MetricConfig;
 import org.apache.flink.metrics.MetricGroup;
 import org.apache.flink.metrics.reporter.InstantiateViaFactory;
 import org.apache.flink.metrics.reporter.MetricReporter;
-import org.apache.flink.runtime.metrics.groups.AbstractMetricGroup;
-import org.apache.flink.runtime.metrics.groups.FrontMetricGroup;
-import org.apache.flink.util.NetUtils;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.annotation.Nullable;
-import javax.management.InstanceAlreadyExistsException;
-import javax.management.InstanceNotFoundException;
-import javax.management.MBeanServer;
-import javax.management.MalformedObjectNameException;
-import javax.management.NotCompliantMBeanException;
-import javax.management.ObjectName;
-import javax.management.remote.JMXConnectorServer;
-import javax.management.remote.JMXServiceURL;
-import javax.management.remote.rmi.RMIConnectorServer;
-import javax.management.remote.rmi.RMIJRMPServerImpl;
-
-import java.io.IOException;
-import java.lang.management.ManagementFactory;
-import java.net.MalformedURLException;
-import java.rmi.NoSuchObjectException;
-import java.rmi.NotBoundException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * {@link MetricReporter} that exports {@link Metric Metrics} to NewRelic.
@@ -71,10 +32,10 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 @InstantiateViaFactory(factoryClassName = "org.apache.flink.metrics.newrelic.NewRelicReporterFactory")
 public class FlinkNewRelicReporter extends ScheduledDropwizardReporter {
-	protected String[] excluded_variables;
+	protected String[] excludedVariables;
 	@Override
 	public ScheduledReporter getReporter(MetricConfig config) {
-		excluded_variables = config.getString(ConfigConstants.METRICS_REPORTER_EXCLUDED_VARIABLES, "").split(";");
+		excludedVariables = config.getString(ConfigConstants.METRICS_REPORTER_EXCLUDED_VARIABLES, "").split(";");
 
 		String apiKey = config.getString("apiKey", null);
 		String appName = config.getString("appName", null);
@@ -95,12 +56,14 @@ public class FlinkNewRelicReporter extends ScheduledDropwizardReporter {
 	@Override
 	public void notifyOfAddedMetric(Metric metric, String metricName, MetricGroup group) {
 		boolean toReport = true;
-		for (String exclude: excluded_variables) {
+		for (String exclude: excludedVariables) {
 			if (group.getMetricIdentifier(metricName).startsWith(exclude)) {
 				toReport = false;
 				break;
 			}
 		}
-		if (toReport) super.notifyOfAddedMetric(metric, metricName, group);
+		if (toReport) {
+			super.notifyOfAddedMetric(metric, metricName, group);
+		}
 	}
 }
